3a9fe4e1742dc8352cc3e623d60a834a323ce471
Eldar Gashimov <eldar.gashimov@inbox.ru>
2020-10-13 21:10:10 +0300

    Add LaunchScreen

---
+++ goodies/goodies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goodies
+//
+//  Created by Mac on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
