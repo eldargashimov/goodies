78d0cec0a11f26af54c1093d85b43d2bbd726ffa
Eldar Gashimov <eldar.gashimov@inbox.ru>
2020-10-16 01:41:42 +0300

    Add animation for loading the application and table for the home screen

--- goodies/goodies/ViewController.swift
+++ goodies/goodies/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        logoLabel.alpha = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            UIView.animate(withDuration: 0.5) {
+                self.logoLabel.textColor = .systemGreen
+                self.logoLabel.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+                self.logoLabel.alpha = 1
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    UIView.animate(withDuration: 0.5) {
+                        self.logoLabel.transform = .identity
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                            self.performSegue(withIdentifier: "launchForMain", sender: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
